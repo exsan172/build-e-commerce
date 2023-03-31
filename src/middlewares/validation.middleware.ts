341301141bcd238cab7ff720a4187138736ae296
Exsan Renaldhi <exsan172@gmail.com>
2023-03-31 12:35:39 +0700

    fix on failed validation remove images

--- src/middlewares/validation.middleware.ts
+++ src/middlewares/validation.middleware.ts
@@ -1,8 +1,9 @@
 import config from '../configs/index.config';
 import { Request, Response, NextFunction } from 'express';
 import { validationResult } from 'express-validator';
+import { deleteFile } from './upload.middleware';
 
-const validationMiddleware = (req: Request, res: Response, next: NextFunction) => {
+const validationMiddleware = async (req: Request, res: Response, next: NextFunction) => {
     const errors = validationResult(req);
     if (errors.isEmpty()) {
         return next();
@@ -15,6 +16,10 @@ const validationMiddleware = (req: Request, res: Response, next: NextFunction) =
             message : data.msg
         })
     })
+    
+    if(req.body.public_id !== undefined) {
+        await deleteFile(req.body.public_id)
+    }
     
     return config.response(res, 442, false, "validasi input", [], message)
 };
